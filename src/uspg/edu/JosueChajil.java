f24501740ef0a9d1a15fe09208961d7d095f403f
josue chajil <josuejouse7@gmail.com>
2020-08-22 17:22:55 -0600

    documentacion ilegal

---
+++ src/uspg/edu/JosueChajil.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uspg.edu;
+
+/**
+ *
+ * @author DELL
+ */
+public class JosueChajil {
+    private String nombre;
+    private String apellido;
+    private String fecha_nacimiento;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getFecha_nacimiento() {
+        return fecha_nacimiento;
+    }
+
+    public void setFecha_nacimiento(String fecha_nacimiento) {
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+    
+    
+     public void presentacion () {
+    System.out.println("Gusto saludar mi nombre es:" + getNombre());
+    System.out.println("mi apellido es:" + getApellido());
+    System.out.println("mi fecha de nacimiento es:" + getFecha_nacimiento());
+    System.out.println("Me gusta el futbol");
+}
+         }
+
